c2fc9c75da279e97d8cd6407e79fdf511e29fc91
ADigitalNative <adigitalnative@gmail.com>
2012-05-23 17:43:55 -0400

    Removing unused method from model/image_validation.rb

--- app/models/image_validations.rb
+++ app/models/image_validations.rb
@@ -1,7 +1,4 @@
 module ImageValidations
-  def self.allow_amazon_uploads(params=true)
-    allow_amazon_uploads = true
-  end
 
   def self.included(source)
     image_validator_regex = /^https?:\/\/(?:[a-z\-\d]+\.)+[a-z]{2,6}(?:\/[^\/#?]+)+\.(?:jpg|gif|png)$/ix
@@ -9,7 +6,7 @@ module ImageValidations
     source.validates_format_of :link, :with => image_validator_regex, message: "URL must start with http and be a .jpg, .gif, or .png"
     source.validates_length_of :link, :within => 3..2048, message: "Given URL needs to be less then 2048 characters"
     source.validates_length_of :comment, :within => 3..256, :allow_blank => true
-    source.after_validation :send_photo_to_amazon if true
+    source.after_validation :send_photo_to_amazon
     source
   end
 end
